cafbb215383df4e5469658c0cdd760b29cf910f6
Dmitri Antonov <Dmitri.Antonov@ru.ibm.com>
2015-09-12 22:13:38 +0300

    trivial changes

--- src/main/scala/com/myapp/MainActivity.scala
+++ src/main/scala/com/myapp/MainActivity.scala
@@ -1,13 +1,32 @@
 package com.myapp
 
+import java.util.Date
+
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.{Button, Toast}
 
-class MainActivity extends Activity {
+class MainActivity extends Activity with View.OnClickListener {
 
-  override def onPostCreate(savedInstanceState: Bundle) {
-    super.onPostCreate(savedInstanceState)
-    setContentView(R.layout.main)
+  var button: Button = null
+
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    button = new Button(this)
+    button.setOnClickListener(this)
+    updateTime()
+    setContentView(button)
   }
 
+  override def onPause() = {
+    super.onPause()
+    Toast.makeText(getBaseContext, "SUDA PODOSHEL!", Toast.LENGTH_LONG).show()
+  }
+
+  override def onClick(view: View): Unit = {
+    updateTime()
+  }
+
+  def updateTime() = button.setText(new Date().toString)
 }
